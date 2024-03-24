25a0b838ad3fd566b2bc77481fa9aeb34df43258
Sergey-Ivanovich <ericnoata@gmail.com>
2024-03-24 16:21:37 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This Project Was Coded By{" "}
+          <a href="https://github.com/Sergey-Ivanovich"> Eric Noata</a> And is
+          Open-Sourced on <a href="/">GitHub</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
