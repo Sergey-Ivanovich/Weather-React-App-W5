7943d0f922b3232946d884de5e92015f3d836115
Sergey-Ivanovich <ericnoata@gmail.com>
2024-03-24 16:54:36 +0000

    installed bootstrap and updated installs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ export default function App() {
         <p>
           This Project Was Coded By{" "}
           <a href="https://github.com/Sergey-Ivanovich"> Eric Noata</a> And is
-          Open-Sourced on <a href="/">GitHub</a>
+          Open-Sourced on{" "}
+          <a href="https://github.com/Sergey-Ivanovich/Weather-React-App-W5">
+            GitHub
+          </a>
         </p>
       </footer>
     </div>
